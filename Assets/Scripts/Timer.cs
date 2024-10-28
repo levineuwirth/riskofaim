7d9c379717455d81fb4f5f53ff44f48a89b279a4
sjkim994 <sjkd2022@mymail.pomona.edu>
2024-10-28 16:58:35 +0100

    added timer + edited target spawner prefab

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -1,22 +1,33 @@
+using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Timer : MonoBehaviour
 {
-    [field: SerializeField] public float timerDuration {get; private set;}
-    [field: SerializeField] public TextMeshPro TimerText {get; private set;}
-    private bool _timerOn = true;
+    [field: SerializeField] public float timerDurationSeconds {get; private set;}
+    [field: SerializeField] public TextMeshProUGUI timerText {get; private set;}
+    private bool _timerActive = true;
+    private float _timeLeft;
+    private TimeSpan timeSpan;
 
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        _timerActive = true;
+        _timeLeft = timerDurationSeconds;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(_timerActive) {
+            _timeLeft -= Time.deltaTime;
+
+            if(_timeLeft <= 0) {
+                _timerActive = false;
+            }
+        }
+
+        timeSpan = TimeSpan.FromSeconds(_timeLeft);
+        timerText.text = timeSpan.Minutes.ToString() + " : " + timeSpan.Seconds.ToString() + " : " + timeSpan.Milliseconds.ToString();
     }
 }
