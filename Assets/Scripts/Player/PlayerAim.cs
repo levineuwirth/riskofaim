7d7872201f0c3c549708b0e8b851694af1d16296
sjkim994 <sjkd2022@mymail.pomona.edu>
2024-11-10 00:51:23 +0100

    fixed cursor behavior during pause

--- Assets/Scripts/Player/PlayerAim.cs
+++ Assets/Scripts/Player/PlayerAim.cs
@@ -12,18 +12,32 @@ public class PlayerAim : MonoBehaviour
 
     void Start()
     {
+    }
+
+    void Update() {
+        if(Time.timeScale != 0) {
+            LockCursor();
+
+            _xRotation += Input.GetAxis("Mouse X") * xLookSpeed;
+            _xRotation = _xRotation % 360;
+            _yRotation += -Input.GetAxis("Mouse Y") * yLookSpeed;
+            _yRotation = Mathf.Clamp(_yRotation, -yLookLimit, yLookLimit);
+
+            transform.rotation = Quaternion.Euler(new Vector3 (_yRotation, _xRotation));
+        }
+        else {
+            UnlockCursor();
+        }
+        
+    }
+
+    private void LockCursor() {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
 
-    void Update()
-    {
-        _xRotation += Input.GetAxis("Mouse X") * xLookSpeed;
-        _xRotation = _xRotation % 360;
-        _yRotation += -Input.GetAxis("Mouse Y") * yLookSpeed;
-        _yRotation = Mathf.Clamp(_yRotation, -yLookLimit, yLookLimit);
-
-        transform.rotation = Quaternion.Euler(new Vector3 (_yRotation, _xRotation));
-        
+    private void UnlockCursor() {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
     }
 }
