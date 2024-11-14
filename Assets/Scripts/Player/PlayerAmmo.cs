1bd2c38ee15bcc314748e21cf9c0461626708790
Levi Neuwirth <ln@levineuwirth.org>
2024-11-14 16:48:55 +0100

    rework Ammo

--- Assets/Scripts/Player/PlayerAmmo.cs
+++ Assets/Scripts/Player/PlayerAmmo.cs
@@ -3,18 +3,21 @@ using UnityEngine;
 
 public class PlayerAmmo : MonoBehaviour
 {
+    [field: SerializeField] public int nonConsumeChance {get; private set;}
+    [field: SerializeField] public float reloadTime {get; private set;}
+    [field: SerializeField] public float reloadTimeMultiplier {get; private set;}
+    
     public float maxCapacity;
     private float currentCapacity;
     private bool _isReloading;
-    private float reloadTime;
-    private float reloadTimeMultipler;
-
+    
     void Start()
     {
         reloadTime = 5; // need to playtest where we should start this
-        reloadTimeMultipler = 1; //placeholder
+        reloadTimeMultiplier = 1; //placeholder
         currentCapacity = maxCapacity;
         _isReloading = false;
+	nonConsumeChance = 0;
     }
 
     void Update()
@@ -25,8 +28,13 @@ public class PlayerAmmo : MonoBehaviour
     bool decrementMagazine(){
         if (currentCapacity < 1 || _isReloading){
             return false; // the shot, i.e. raycasts, should NOT be fired 
-        }  
-        currentCapacity -= 1;
+        }
+	// roll for the non-consume chance.
+	System.Random rnd = new System.Random();
+	int roll = rnd.Next(1, 101-nonConsumeChance);
+	if (roll != 1){
+		currentCapacity -= 1;
+	    }
         return true; // the shot, i.e. raycasts, should be fired
     }
 
