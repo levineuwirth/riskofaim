e662c049de7420421a35b0a23c34830a63759c91
Levi Neuwirth <ln@levineuwirth.org>
2024-11-14 16:08:31 +0100

    modifiers

--- Assets/Scripts/Player/PlayerHealth.cs
+++ Assets/Scripts/Player/PlayerHealth.cs
@@ -4,17 +4,24 @@ using UnityEngine;
 public class PlayerHealth : MonoBehaviour
 {
     [field: SerializeField] public int maxHealth {get; private set;}
+    [field: SerializeField] public int baseDamage {get; private set;}
+    [field: SerializeField] public int damageModifier {get; private set;}
+    [field: SerializeField] public int baseHeal {get; private set;}
+    [field: SerializeField] public int healModifier {get; private set;}
     private int currentHealth;
-
     private void Start() {
         currentHealth = maxHealth;
+	PlayerShoot.EOnTargetMiss += takeDamage;
+	damageModifier = 1;
+	baseDamage = 1;
     }
     
-    public void takeDamage(int damage) {
-        currentHealth -= damage;
+    public void takeDamage() {
+        currentHealth -= damageModifier * baseDamage;
+	Debug.Log("DAMAGE!");
     }
-    public void heal(int heal) {
-        currentHealth += heal;
+    public void heal() {
+        currentHealth += healModifier * baseHeal;
     }
     public void increaseMaxHealth(int health) {
         maxHealth += health;
--- Assets/Scripts/Player/PlayerShoot.cs
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -2,7 +2,9 @@ using UnityEngine;
 
 public class PlayerShoot : MonoBehaviour {
     public delegate void OnTargetHit();
+    public delegate void OnTargetMiss();
     public static OnTargetHit EOnTargetHit;
+    public static OnTargetMiss EOnTargetMiss;
     [field: SerializeField] public AudioSource deathSFX {get; private set;}
     [field: SerializeField] public Camera playerCamera {get; private set;}
     private RaycastHit _raycastHit;
@@ -17,8 +19,8 @@ public class PlayerShoot : MonoBehaviour {
                 Destroy(_raycastHit.transform.gameObject);
                 EOnTargetHit?.Invoke();
             } else {
-                // Decrement health logic. For now we just use a backspace placeholder.
-            }
+		EOnTargetMiss?.Invoke();
+	    }
         }
     }
 }
