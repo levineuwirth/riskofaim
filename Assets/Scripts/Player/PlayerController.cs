96f2adf58a57860b9e746e1bd20e52f20a953590
sjkim994 <sjkd2022@mymail.pomona.edu>
2024-11-14 17:06:11 +0100

    settings opens and closes

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -7,6 +7,12 @@ public class PlayerController : MonoBehaviour {
     [field: SerializeField] public KeyCode pause {get; private set;}
     public static PlayerController Instance;
     private void Awake() {
-        Instance = this;
+        if(Instance == null) {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/UIScripts/SettingsController.cs
+++ Assets/Scripts/UIScripts/SettingsController.cs
@@ -1,5 +1,30 @@
 using UnityEngine;
 
 public class SettingsController : MonoBehaviour {
-    
+    [field: SerializeField] public GameObject settingsPanel {get; private set;}
+    private bool _isSettingsOpen;
+
+    private void Start() {
+        settingsPanel.SetActive(false);
+    }
+    private void Update() {
+        if(Input.GetKeyDown(PlayerController.Instance.pause)) {
+            if(!_isSettingsOpen) {
+                OpenSettings();
+                PauseManager.Pause();
+            }
+            else {
+                CloseSettings();
+                PauseManager.Unpause();
+            }
+        }
+    }
+    public void OpenSettings() {
+        settingsPanel.SetActive(true);
+        _isSettingsOpen = true;
+    }
+    public void CloseSettings() {
+        settingsPanel.SetActive(false);
+        _isSettingsOpen = false;
+    }
 }
